705dc9d01e15a716866ca33ef2a06a27a2fe53a4
Ella-Kim913 <hyunkk0913@gmail.com>
2025-04-07 16:47:46 -0700

    homepage:flight info card

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,30 +1,94 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+import { useState, useEffect } from 'react';
+import UpcomingFlight from '@/components/upcomingFlight';
+
+type FlightInfo = {
+  date: string;
+  flightNumber: string;
+  gate: string;
+  departure: string;
+  arrival: string;
+  departureTime: string;
+  arrivalTime: string;
+  duration: string;
+  status: string;
+};
 
 export default function Home() {
+
+  //TODO connetc the backend
+  const user = 'Ryan';
+  const userImage = require('../../assets/images/user-icon.png');
+
+  const [flightInfo, setFlightInfo] = useState<FlightInfo | null>(null);
+
+  useEffect(() => {
+    const temp: FlightInfo = {
+      date: '2023-10-01',
+      flightNumber: 'AC123',
+      gate: 'A1',
+      departure: 'SEA',
+      arrival: 'LAX',
+      departureTime: '7:20 AM',
+      arrivalTime: '10:05 AM',
+      duration: '2h 45m',
+      status: 'DELAYED',
+    };
+
+    setFlightInfo(temp);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="../about" style={styles.button}>
-        Go to About screen
-      </Link>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>Hello {user}</Text>
+          <Text style={styles.subtitle}>How can I help you?</Text>
+        </View>
+        <Image source={userImage} style={styles.userImage} />
+      </View>
+
+      <View style={styles.section}>
+        <UpcomingFlight flightInfo={flightInfo} />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  // Layout
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#fff',
+    paddingHorizontal: 30,
+  },
+  section: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+
+  // Header
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
   },
-  text: {
-    color: '#fff',
+  userImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+
+  // Text
+  title: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  subtitle: {
+    color: '#000',
   },
 });
