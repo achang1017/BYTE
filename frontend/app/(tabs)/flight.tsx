e28b1750aa5b8f8c2e17a6073f422c3dcc8086bc
Ella-Kim913 <hyunkk0913@gmail.com>
2025-04-18 13:43:03 -0700

    flights page

--- frontend/app/(tabs)/flight.tsx
+++ frontend/app/(tabs)/flight.tsx
@@ -1,21 +1,40 @@
-import { Text, View, StyleSheet } from 'react-native';
+import FlightTracker from '@/components/flightTracker';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
 
 export default function FlightScreen() {
+    const flightInfo = {
+        date: '2023-10-01',
+        departure: 'LAX',
+        departureTime: '12:00 PM',
+        arrival: 'JFK',
+        arrivalTime: '8:00 PM',
+        flightNumber: 'AA123',
+        gate: 'A1',
+        status: 'On Time',
+        duration: '6h 0m'
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>About screen</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            {/* Detail flight section */}
+            <View style={styles.section}>
+                <FlightTracker flightInfo={flightInfo} ></FlightTracker>
+
+            </View>
+
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    // Layout
     container: {
         flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: '#fff',
+        paddingHorizontal: 30,
     },
-    text: {
-        color: '#fff',
+    section: {
+        marginTop: 20,
+        marginBottom: 20,
     },
 });
