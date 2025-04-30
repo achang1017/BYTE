a1ada3b2c17acb259b0f0a7c045cedc3f717d434
BrianChiang2004 <930223brian@gmail.com>
2025-04-29 22:40:11 -0700

    Add Firestore user preference fetch functionality

--- backend/api/routes/firestoreRoutes.js
+++ backend/api/routes/firestoreRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getCollection } = require('../controllers/firestoreController');
+const { getCollection } = require('../../controllers/firestoreController');
+
 const router = express.Router();
 
 router.get('/collection/:name', getCollection);
