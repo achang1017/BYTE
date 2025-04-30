ceb838b1131500e5b70db846d3c19ef139353c46
BrianChiang2004 <930223brian@gmail.com>
2025-04-29 21:25:00 -0700

    Connect Firestore to fetch user preferences

---
+++ backend/api/routes/firestoreRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getCollection } = require('../controllers/firestoreController');
+const router = express.Router();
+
+router.get('/collection/:name', getCollection);
+
+module.exports = router;
